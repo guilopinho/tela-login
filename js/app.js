6207d8ff5aa6246464de97255cd87aadc90fb737
Guilherme Lopes <100242805+guilopinho@users.noreply.github.com>
2022-05-08 12:49:54 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,4 +7,12 @@ form.addEventListener('submit',function(e){
     setInterval(() => {
         form.classList.add("fade-out");
     },1000);
-})
+});
+
+// Dark Mode
+
+let check = document.getElementById('check');
+check.addEventListener('click',()=>{
+    let body = document.querySelector('body');
+    body.classList.toggle('dark-mode');
+});
